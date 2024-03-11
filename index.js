0a78ce314917f42a6ca1f9ea609c53471e75abf8
Lethabo Kgoele <lethabotshepojr@gmail.com>
2024-03-11 18:22:07 +0200

    Done
    Final

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-let aways = 0
-let homes = 0
-
-
-
+let awayscore = 0
+let homescore = 0
 let away = document.getElementById('awayscore')
 let home = document.getElementById('homescore')
-console.log(away)
-console.log(home)
+
+function awayFT() {
+    awayscore += 1
+    away.textContent = awayscore
+}
+
+function awayKD() {
+    awayscore += 2
+    away.textContent = awayscore
+}
+
+function awayThree() {
+    awayscore += 3
+    away.textContent = awayscore
+}
+
+function homeFT() {
+    homescore += 1
+    home.textContent = homescore
+}
+
+function homeKD() {
+    homescore += 2
+    home.textContent = homescore
+}
+
+function homeThree() {
+    homescore += 3
+    home.textContent = homescore
+}
+
 
