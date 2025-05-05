ec1e6e5843965c4ca9461c9507a0b0c9eea06e7a
Lethabo Kgoele <lethabotshepojr@gmail.com>
2025-05-06 00:48:27 +0200

    added type on button and prettier added

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
-let awayscore = 0
-let homescore = 0
-let away = document.getElementById('awayscore')
-let home = document.getElementById('homescore')
+let awayscore = 0;
+let homescore = 0;
+let away = document.getElementById("awayscore");
+let home = document.getElementById("homescore");
 
 function awayFT() {
-    awayscore += 1
-    away.textContent = awayscore
+  awayscore += 1;
+  away.textContent = awayscore;
 }
 
 function awayKD() {
-    awayscore += 2
-    away.textContent = awayscore
+  awayscore += 2;
+  away.textContent = awayscore;
 }
 
 function awayThree() {
-    awayscore += 3
-    away.textContent = awayscore
+  awayscore += 3;
+  away.textContent = awayscore;
 }
 
 function homeFT() {
-    homescore += 1
-    home.textContent = homescore
+  homescore += 1;
+  home.textContent = homescore;
 }
 
 function homeKD() {
-    homescore += 2
-    home.textContent = homescore
+  homescore += 2;
+  home.textContent = homescore;
 }
 
 function homeThree() {
-    homescore += 3
-    home.textContent = homescore
+  homescore += 3;
+  home.textContent = homescore;
 }
-
-
